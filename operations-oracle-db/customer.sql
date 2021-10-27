e89cc060d5e985db4abcfc66aa74c4efa1dfb6ad
Jose <alonsomotta@gmail.com>
2021-10-27 12:40:45 +0200

    Cambiado insert por INSERT

--- operations-oracle-db/customer.sql
+++ operations-oracle-db/customer.sql
@@ -1,5 +1,5 @@
-insert into customer(id,name) values(1,'Andrea');
-insert into customer(id,name) values(2,'Jose');
-insert into customer(id,name) values(3,'Alejandro');
+INSERT into customer(id,name) values(1,'Andrea');
+INSERT into customer(id,name) values(2,'Jose');
+INSERT into customer(id,name) values(3,'Alejandro');
 commit;
 
